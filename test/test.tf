4cf997855f1c493ffc5cfacf13ab4a7c3e106d92
Henryk Konsek <hekonsek@gmail.com>
2021-07-19 14:05:44 +0200

    User should be provided as input.

--- test/test.tf
+++ test/test.tf
@@ -3,9 +3,11 @@ module "test" {
 
   name = "testrole"
   users = ["henryk.test"]
-  users_arn_map = {"henryk.sso" : "arn:aws:iam::001234567890:user/henryk.sso"}
+  users_arn_map = {"henryk.test" : var.user_arn}
   policies_arns = ["arn:aws:iam::aws:policy/AlexaForBusinessFullAccess"]
 }
+
+variable "user_arn" {}
 
 output "output" {
   value = module.test.role
